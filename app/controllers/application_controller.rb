5f7c3cc87678296be6f737fe8dba39056f26409a
pardeep <pardeep.dhingra@sial.com>
2016-02-16 21:31:25 +0530

    sign up and sign in template

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,16 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :authenticate_user!
+
+  layout :layout_by_resource
+
+  protected
+
+  def layout_by_resource
+  	if devise_controller?
+  		"user_for_devise"
+  	else
+  		"application"
+  	end
+  end
 end
